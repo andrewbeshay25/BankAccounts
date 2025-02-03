3fbb0ba75ac6e1d902bc9ab1955602768d09454a
Andrew Beshay <102611183+andrewbeshay25@users.noreply.github.com>
2025-02-03 15:28:48 -0500

    Update SavingsAccount.java

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,19 +1,15 @@
-public class SavingsAccount {
-    private int accountNumber;
-    private double balance;
+class SavingsAccount {
+    private double balance = 0;
 
-    public SavingsAccount(int accountNumber) {
-        this.accountNumber = accountNumber;
-        this.balance = 0;
-    }
+    public double getBalance() { return balance; }
 
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-    public double getBalance() {
-        return balance;
-    }
-    public void setBalance(double balance) {
-        this.balance = balance;
+    public void deposit(double amount) { balance += amount; }
+
+    public boolean withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            return true;
+        }
+        return false;
     }
 }
